9477b328f46ff794140938779b096407e5bb9ea5
1UC1F3R616 <kushchoudhary8@gmail.com>
2020-10-09 02:08:01 +0530

    add: question model

--- src/models/event.js
+++ src/models/event.js
@@ -13,7 +13,6 @@ const eventSchema = new mongoose.Schema({
     },
     description: {
         type: String,
-        required: true
     }
 })
 
--- src/models/question.js
+++ src/models/question.js
@@ -1,5 +1,22 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
+const questionSchema = new mongoose.Schema({
+    event_id : {
+        type: mongoose.ObjectId,
+        required: true
+    },
+    statement : {
+        type : String,
+        required: true
+    },
+    number : {
+        type : Integer,
+        required : true
+    }
+})
+
+const Question = mongoose.model('Question', questionSchema)
+module.exports = Question
