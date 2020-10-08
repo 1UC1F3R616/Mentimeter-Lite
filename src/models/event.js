8af49a1a3023abb163840dd8eb3cd6a5cd80458f
1UC1F3R616 <kushchoudhary8@gmail.com>
2020-10-09 01:17:40 +0530

    ad: event model

--- src/models/event.js
+++ src/models/event.js
@@ -1,5 +1,21 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
 
+const eventSchema = new mongoose.Schema({
+    user_id : {
+        type: ObjectId,
+        required: true
+    },
+    name : {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    }
+})
+
+const Event = mongoose.model('Event', eventSchema)
+module.exports = Event
